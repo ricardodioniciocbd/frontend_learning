88be71816910dfa7fbdcf79d9af4b363cfd7bdae
Ricardo Dionicio <dricardooc4@gmail.com>
2026-02-18 21:41:33 -0600

    Implementación de componentes y estilos del apartado de Trabajos

--- projects/Portafolio/src/App.jsx
+++ projects/Portafolio/src/App.jsx
@@ -1,15 +1,17 @@
 import Header from "./components/layout/Header";
 import Hero from  "./components/layout/Hero";
 import Clientes from "./components/layout/Clientes";
+import Trabajos from "./components/layout/Trabajos";
 
 function App() {
   
   return(<>
   
     <div className="contenedor">  {/* Contenedor para poder centrar todos los elementos de forma horizontal en nuestra pagina */}
-      <Header/>
-      <Hero/>
-      <Clientes/>
+      <Header />
+      <Hero />
+      <Clientes />
+      <Trabajos />
     </div>
   </>) 
   
